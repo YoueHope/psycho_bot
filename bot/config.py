4b850cac452c6afc461add44c8b30f595dee161c
dariprim <dariprim@Mac.lan>
2026-03-11 16:19:56 -0700

    рабочая версия на yandexgpt5 без бд и дообучения

---
+++ bot/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+MODEL_PATH = os.getenv("MODEL_PATH", "./models/YandexGPT-5-Lite-8B-instruct-Q4_K_M.gguf") 
+SENTIMENT_MODEL = os.getenv("SENTIMENT_MODEL", "blanchefort/rubert-base-cased-sentiment")
